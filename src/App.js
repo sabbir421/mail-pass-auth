871cef36e6ef4c64bb8e86daa93966e1b93903b7
sabbir421 <sabbiralazim421@gmail.com>
2022-01-20 00:40:00 +0600

    mail pass

--- src/App.js
+++ src/App.js
@@ -1,23 +1,57 @@
-import logo from './logo.svg';
+
+import { useState } from 'react';
 import './App.css';
+import firebaseInit from './Firebase/init';
+import { getAuth, createUserWithEmailAndPassword } from "firebase/auth";
+
+firebaseInit();
+
+
+
+
 
 function App() {
+  const auth = getAuth();
+
+const [email, setEmail]=useState('');
+const [password, setPassword]=useState('');
+const [error,setError]=useState('')
+const getEmail=e=>{
+  setEmail(e.target.value);
+}
+
+const getPassword=e=>{
+  setPassword(e.target.value);
+}
+
+const emailHandel=e=>{
+  e.preventDefault()
+ console.log(email,password);
+ if(password.length<6){
+   setError('password must be 6 carecters')
+   return; 
+ }
+ createUserWithEmailAndPassword(auth, email, password)
+ .then((userCredential) => {
+  // Signed in 
+  const user = userCredential.user;
+  console.log(user);
+  setError('')
+  // ...
+})
+ 
+}
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+     <form onSubmit={emailHandel} action="">
+       <label htmlFor="">email</label>
+       <input onBlur={getEmail} type="text" />
+       <label htmlFor="">password</label>
+       <input onBlur={getPassword} type="text" />
+       <input type="submit" />
+       <div>{error}</div>
+     </form>
     </div>
   );
 }
---
+++ src/Firebase/config.js
@@ -0,0 +1,9 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyAynwlNXizHu4MtSZkUZZyGj9_vAG9rZhg",
+    authDomain: "fir-practice-6e358.firebaseapp.com",
+    projectId: "fir-practice-6e358",
+    storageBucket: "fir-practice-6e358.appspot.com",
+    messagingSenderId: "955509271385",
+    appId: "1:955509271385:web:10a00f86a81f46a07fba48"
+  };
+  export default firebaseConfig;
